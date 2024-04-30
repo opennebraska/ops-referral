baa4f97edb0c0af9fe877b071e1fc6730c2c14f0
Jay Hannah <jay.hannah@iinteractive.com>
2024-04-30 16:00:37 -0500

    oh lol sigh I forgot execute()

--- membership.pl
+++ membership.pl
@@ -2,11 +2,10 @@
 
 use 5.38.0;
 use DBI;
-use Data::Printer;
 
 my $dbh = DBI->connect("dbi:SQLite:dbname=ops.sqlite3","","") or die $DBI::errstr;
 my $strsql = <<SQL;
-  DROP TABLE IF EXISTS membership;
+  DROP TABLE IF EXISTS membership
 SQL
 $dbh->do($strsql) or die $DBI::errstr;
 $strsql = <<SQL;
@@ -21,24 +20,20 @@ SQL
 $dbh->do($strsql) or die $DBI::errstr;
 
 $strsql = <<SQL;
-  SELECT *
-  FROM membership_raw
+  SELECT * FROM membership_raw
 SQL
 my $sth1 = $dbh->prepare($strsql) or die $DBI::errstr;
-# $strsql = <<SQL;
-#   INSERT INTO membership (school, grade, RaceEthnicity, gender, students) VALUES
-#   (?, ?, ?, ?, ?)
-# SQL
-# my $sth2 = $dbh->prepare($strsql) or die $DBI::errstr;
+$strsql = <<SQL;
+  INSERT INTO membership (school, grade, RaceEthnicity, gender, students) VALUES
+  (?, ?, ?, ?, ?)
+SQL
+my $sth2 = $dbh->prepare($strsql) or die $DBI::errstr;
 
-say "hi1";
-while (my @row = $sth1->fetchrow) {
-  say "hi2";
-  p @row;
-  #if ($row->{'AA-F'}) {
-  #  say $row->{'AA-F'};
-  #  $sth2->execute($row->{school}, $row->{grade}, "African American", "F", $row->{'AA-F'});
-  #}
+$sth1->execute() or die $DBI::errstr;
+while (my $row = $sth1->fetchrow_hashref) {
+  if ($row->{'AA-F'}) {
+    $sth2->execute($row->{school}, $row->{grade}, "African American", "F", $row->{'AA-F'});
+  }
 }
 
 say "Done";
