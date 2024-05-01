9ef532e3e6ac5ae303ebea0fa53e282d4854848a
Jay Hannah <jay.hannah@iinteractive.com>
2024-05-01 17:36:44 -0500

    Use Perl's clustering. Various aesthetics.

--- cluster.pl
+++ cluster.pl
@@ -4,6 +4,16 @@ use 5.38.0;
 use DBI;
 
 my $dbh = DBI->connect("dbi:SQLite:dbname=ops.sqlite3","","");
+
+$dbh->do("DROP TABLE IF EXISTS disc_cluster");
+$dbh->do("
+  CREATE TABLE disc_cluster (
+    referral_count TEXT,
+    RaceEthnicity TEXT,
+    students INT
+  )
+");
+
 my $strsql = <<SQL;
   WITH repeat_referrals AS (
     SELECT RaceEthnicity, count(*) referral_count
@@ -15,11 +25,17 @@ my $strsql = <<SQL;
   WHERE referral_count > 1
   GROUP BY 1,2;
 SQL
-my $sth = $dbh->prepare($strsql);
-$sth->execute;
+my $sth1 = $dbh->prepare($strsql);
+$sth1->execute;
+
+$strsql = <<SQL;
+  INSERT INTO disc_cluster (referral_count, RaceEthnicity, students)
+  VALUES (?, ?, ?)
+SQL
+my $sth2 = $dbh->prepare($strsql);
 
 my $counts = {};
-while (my @row = $sth->fetchrow) {
+while (my @row = $sth1->fetchrow) {
   if ($row[0] >= 2)  { $counts->{"02 or more"}->{$row[1]} += $row[2] || 0 }
   if ($row[0] >= 5)  { $counts->{"05 or more"}->{$row[1]} += $row[2] || 0 }
   if ($row[0] >= 10) { $counts->{"10 or more"}->{$row[1]} += $row[2] || 0 }
@@ -30,6 +46,7 @@ while (my @row = $sth->fetchrow) {
 foreach my $count (keys %$counts) {
   foreach my $race (keys %{$counts->{$count}}) {
     say "$count|$race|" . $counts->{$count}->{$race};
+    $sth2->execute($count, $race, $counts->{$count}->{$race});
   }
 }
 
--- generate_website.py
+++ generate_website.py
@@ -12,6 +12,8 @@ import seaborn as sns  # https://seaborn.pydata.org
 
 import sqlite3
 
+raceEthnicity = ['African American', 'Asian', 'Hispanic', 'Multi Racial', 'Native American', 'Pacific Islander', 'White']
+
 con = sqlite3.connect("ops.sqlite3")
 
 sqlstr = """
@@ -50,7 +52,15 @@ df3 = pd.read_sql_query(sqlstr, con)
 print(df3.head())
 df3 = df3.pivot_table(index='referral_count', columns='RaceEthnicity', values='students')
 print(df3.head())
-sns_plot = sns.lineplot(data=df3)  # , x="students", y="referral_count")
+
+# Hmmm... can't seem to do what cluster.pl does. Give up and use that instead...?
+# df3['bucket'] = df3.qcut(
+#      ... https://practicaldatascience.co.uk/machine-learning/how-to-bin-or-bucket-customer-data-using-pandas
+#   df3['referral_count'],
+#   q=[pivot_table(index='referral_count', columns='RaceEthnicity', values='students')
+# print(df3.head())
+
+sns_plot = sns.lineplot(data=df3)
 sns_plot.set_xlabel("Referral Count")
 sns_plot.set_ylabel("Students")
 sns_plot.set_xscale("log")
@@ -58,18 +68,40 @@ sns_plot.set_yscale("log")
 plt.savefig('d3.png', bbox_inches='tight')
 plt.clf()
 
+sqlstr = """
+  SELECT *
+  FROM disc_cluster
+"""
+df4 = pd.read_sql_query(sqlstr, con)
+print(df4.head())
+df4 = df4.pivot_table(index='referral_count', columns='RaceEthnicity', values='students')
+print(df4.head())
+print(df4.columns)
+# Change to Pandas int to get rid of all the ".0"s
+for r in raceEthnicity:
+  df4[r] = df4[r].astype('Int64')  # capital I
+print(df4.head())
+sns_plot = sns.lineplot(data=df4)
+sns_plot.set_xlabel("Referral Count")
+sns_plot.set_ylabel("Students")
+plt.savefig('d4.png', bbox_inches='tight')
+plt.clf()
+
+
 
 with document(title='Omaha Public Schools Referral (Disciplinary) Data Analysis') as doc:
-  h1('Omaha Public Schools Referral (Disciplinary) Data Analysis')
-  h2('2018-2019 School Year')
-  h3('Students')
+  h1('Omaha Public Schools 2018-2019')
+  raw('Referral (disciplinary) data analysis. <a href="https://github.com/opennebraska/ops-referral">[Source code]</a>')
+  h2('Students')
   raw(df1.to_html(index=False))
   raw('<img src="d1.png">')
-  h3('Total Referrals')
+  h2('Total Referrals')
   raw(df2.to_html(index=False))
   raw('<img src="d2.png">')
-  h3('Number of referrals (> 0) per student')
-  raw(df3.to_html())
+  h2('Number of referrals > 0')
+  # raw(df3.to_html())
+  raw(df4.to_html())
+  raw('<img src="d4.png">')
   raw('<img src="d3.png">')
 
   # for path in photos:
